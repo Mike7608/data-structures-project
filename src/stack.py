d2d16676fc36ffc38abf6b55a74a56ed78dbb54a
Mike Kushner <mike760807@gmail.com>
2023-08-27 17:25:56 +0300

    edit stack.py

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,7 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
@@ -23,7 +24,8 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        node = Node(data, self.top)
+        self.top = node
 
     def pop(self):
         """
@@ -31,4 +33,10 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.top is None:
+            return None
+        else:
+            pop_node = self.top
+            self.top = self.top.next_node
+            pop_node.next_node = None
+            return pop_node.data
