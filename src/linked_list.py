dae5d317d4d094e01b700b2a20dab6b956bdff9d
Mike Kushner <mike760807@gmail.com>
2023-09-09 23:25:20 +0300

    create/edit linked_list.py

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,35 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data: dict):
+        """
+        Конструктор класса Node
+
+        """
+        self.data = data
+        self.next = None
 
 
 class LinkedList:
     """Класс для односвязного списка"""
+    def __init__(self):
+        self.head = None
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node = Node(data)
+        node.next = self.head
+        self.head = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -23,7 +40,7 @@ class LinkedList:
         ll_string = ''
         while node:
             ll_string += f' {str(node.data)} ->'
-            node = node.next_node
+            node = node.next
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.strip()
